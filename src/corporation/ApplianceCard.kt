35baf8d9f1620ac6adc2428438932ea81f82a3f3
Karen Bolaños <dayana0212b@gmail.com>
2025-11-25 10:58:57 -0500

    Added ProductType

--- src/corporation/ApplianceCard.kt
+++ src/corporation/ApplianceCard.kt
@@ -4,8 +4,8 @@ class ApplianceCard(
     name: String,
     brand: String,
     price: Int,
-    val vatios: Int
-): ProductCard(name = name, brand = brand, price = price) {
+    val vatios: Int,
+): ProductCard(name = name, brand = brand, price = price, ProductType.APPLIANCE) {
     override fun printInfo() {
         super.printInfo()
         print("Vatios: $vatios")
--- src/corporation/ProductCard.kt
+++ src/corporation/ProductCard.kt
@@ -4,9 +4,10 @@ package corporation
      val name: String,
      val brand: String,
      val price: Int,
+     val productType: ProductType
      ) {
 
     open fun printInfo (){
-        println("Nombre: $name Marca: $brand Precio: $price")
+        println("Nombre: $name Marca: $brand Precio: $price Tipo de producto: ${productType.title}")
     }
 }
