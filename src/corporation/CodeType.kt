3e49a1a2f2786e609a53bc982fdc1b4493861d23
Karen Bolaños <dayana0212b@gmail.com>
2025-11-26 11:47:27 -0500

    Added Actions regarding employee

--- src/corporation/CodeType.kt
+++ src/corporation/CodeType.kt
@@ -4,5 +4,8 @@ enum class CodeType(val title: String) {
     EXIT("Salir"),
     ENTER_PRODUCT("Ingresar producto"),
     SHOW_ALL_ITEMS("Mostrar todos los productos"),
-    REMOVE_PRODUCT_CARD("Eliminar tarjeta de producto")
+    REMOVE_PRODUCT_CARD("Eliminar tarjeta de producto"),
+    REGISTER_NEW_EMPLOYEE("Registrar nuevo empleado"),
+    FIRE_EMPLOYEE("Despedir un empleado"),
+    SHOW_ALL_EMPLOYEES("Mostrar lista de empleados")
 }
