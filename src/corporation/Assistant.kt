c3d14f03810698bacc9b053c0373b198840aa4c8
Karen Bolaños <dayana0212b@gmail.com>
2025-11-26 11:44:31 -0500

    Added id

--- src/corporation/Assistant.kt
+++ src/corporation/Assistant.kt
@@ -1,9 +1,10 @@
 package corporation
 
 class Assistant(
+    id: Int,
     name: String,
     age: Int
-): Worker(name = name, age = age) {
+): Worker(id = id, name = name, age = age, position = Position.ASSISTANT) {
 
     override fun work() {
         println("Sirvo cafe")
